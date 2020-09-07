6a64a7a4a30d7a2385bcc2ec9601e840ce271183
Tom ODay <tomoday91@gmail.com>
2020-09-07 18:39:38 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,4 +1,15 @@
 def join_nested_strings(src)
   joined = ""
-  row_index
+  row_index = 0
+  while row_index < src.length do
+    element_index = 0
+    while element_index < src[row_index].length do
+      if src[row_index][element_index].is_a? String 
+        joined.concat(src[row_index][element_index])
+      end
+      element_index += 1
+    end
+    row_index += 1
+  end
+  joined
 end
